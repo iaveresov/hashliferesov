e7b909f90473068f29b4500fd5d4039d4796f220
iaveresov <128240770+iaveresov@users.noreply.github.com>
2026-03-24 02:57:30 +0300

    add fsm and tests for plaintext reader

--- src/plaintext.c
+++ src/plaintext.c
@@ -1,12 +1,95 @@
+#include <asm-generic/errno-base.h>
 #include <errno.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/param.h>
 
 #include "plaintext.h"
-
 #define PLAINTEXT_BUF_SIZE 8
+
+enum _FSM_STMT
+{
+  START = 0,
+  COMMENT,
+  NL_COMMENT,
+  PATTERN,
+  NL_PATTERN,
+  ERROR
+};
+
+typedef struct STMT
+{
+  int stmt_num; /*statement; value from enum*/
+  bool unget;   /*bool flag indicating whether to read new char*/
+} STMT;
+
+static void
+_update_sizes (size_t *length, size_t *width, size_t *height)
+{
+  (*height)++;
+  (*width) = MAX (*length, *width);
+  *length = 0;
+}
+
+static void
+_read_pattern_chunk (STMT *stmt, char c, size_t *length, size_t *width,
+                     size_t *height)
+{
+  switch (c)
+    {
+    case '.':
+    case 'O':
+      (*length)++;
+      stmt->stmt_num = PATTERN;
+      break;
+
+    case '\r':
+      stmt->stmt_num = NL_PATTERN;
+      _update_sizes (length, width, height);
+      break;
+
+    case '\n':
+      stmt->stmt_num = PATTERN;
+      _update_sizes (length, width, height);
+      break;
+
+    default:
+      stmt->stmt_num = ERROR;
+    }
+}
+
+static void
+_check_newline (STMT *stmt, char c)
+{
+  stmt->unget = (c == '\n') ? (false) : (true);
+  if (stmt->stmt_num == NL_PATTERN)
+    {
+      stmt->stmt_num = PATTERN;
+    }
+  else if (stmt->stmt_num == NL_COMMENT)
+    {
+      stmt->stmt_num = START;
+    }
+}
+
+static void
+_read_comment_chunk (STMT *stmt, char c)
+{
+  switch (c)
+    {
+    case '\r':
+      stmt->stmt_num = NL_COMMENT;
+      break;
+    case '\n':
+      stmt->stmt_num = START;
+      break;
+    default:
+      stmt->stmt_num = COMMENT;
+      break;
+    }
+}
 
 BYTESBUFFER_T *
 Plaintext_read (char *path)
@@ -23,48 +106,67 @@ Plaintext_read (char *path)
       height = 0;   /* number of pattern file lines excluding comments */
   size_t length = 0, read;
   char *ptr, c;
-  bool in_string = false;
-  bool in_comment = false;
-  // size_t pattern_begin = 0; /* seek point */
 
-  /* TODO: it allows mixing comments and pattern lines which is not claimed as
-     legitimate format in https://conwaylife.com/wiki/Plaintext
-     If we cope with realloc some previous buffers if width changed, we can
-     implement one-pass giving pattern data, not only counting measures.
-     It's also required to get width of the first pattern line before allocation which is doable so:
-      1. skip comments, store file position of pattern start
-      2. look for '\n' counting bytes
-      3. seek back to pattern start
-      4. process file in one-pass
-     */
+  STMT *stmt = (STMT *)malloc (sizeof (STMT)); /*statement struct for FSM*/
+  stmt->stmt_num = START;
+  stmt->unget = false;
+
   while ((read = fread (line, 1, PLAINTEXT_BUF_SIZE, text)))
     {
       ptr = line;
+      stmt->unget = false;
+
       while (ptr < line + read)
         {
-          c = *(ptr++);
-          if (c == '\n' || c == '\r')
+          if (!stmt->unget)
             {
-              if (in_string)
-                {
-                  in_string = false;
-                  if (!in_comment)
-                    {
-                      height++;
-                      width = MAX (width, length);
-                    }
-                }
+              c = *(ptr++);
             }
           else
             {
-              if (!in_string)
+              c = *ptr;
+              stmt->unget = false;
+            }
+
+          switch (stmt->stmt_num)
+            {
+            case START:
+              switch (c)
                 {
-                  length = 1;
-                  in_string = true;
-                  in_comment = c == '!';
+                case '!':
+                  stmt->stmt_num = COMMENT;
+                  break;
+                case '.':
+                case 'O':
+                case '\n':
+                case '\r':
+                  stmt->stmt_num = PATTERN;
+                  stmt->unget = true;
+                  break;
+                default:
+                  stmt->stmt_num = ERROR;
+                  break;
                 }
-              else
-                length++;
+              break;
+
+            case COMMENT:
+              _read_comment_chunk (stmt, c);
+              break;
+
+            case PATTERN:
+              _read_pattern_chunk (stmt, c, &length, &width, &height);
+              break;
+
+            case NL_COMMENT:
+            case NL_PATTERN:
+              _check_newline (stmt, c);
+              break;
+
+            case ERROR:
+              fclose (text);
+              errno = EINVAL;
+              perror ("plaintext");
+              exit (errno);
             }
         }
     }
@@ -77,9 +179,9 @@ Plaintext_read (char *path)
 
   printf ("max_line_len: %lu\n", width);
   printf ("linenum: %lu\n", height);
-
   // TODO: now init BYTESBUFFER with this params
 
+  free (stmt);
   free (line);
 
   return NULL;
--- test/all.sh
+++ test/all.sh
@@ -11,3 +11,7 @@ tst glider.cells
 tst test.cells
 tst test2.cells
 tst test3.cells
+tst test4.cells
+tst test5.cells
+tst test6.cells
+tst test7.cells
